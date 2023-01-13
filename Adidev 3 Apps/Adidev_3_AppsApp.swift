db071eb641b9ba58e2f94c9399e1bd798e73ced4
Joshua Boelman <joshcboelman@gmail.com>
2023-01-13 12:53:18 -0800

    Initial Commit

---
+++ Adidev 3 Apps/Adidev_3_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adidev_3_AppsApp.swift
+//  Adidev 3 Apps
+//
+//  Created by Joshua Boelman on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Adidev_3_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
